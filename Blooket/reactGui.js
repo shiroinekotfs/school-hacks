083a0d1fe413140dfb23206dc88cd46d88f19ab9
白い猫 <shiroineko.tfs@gmail.com>
2023-10-21 22:07:11 +0700

    update credit

--- Blooket/reactGui.js
+++ Blooket/reactGui.js
@@ -1,13 +1,13 @@
 /**
 * @license StewartPrivateLicense-2.0.1
-* Copyright (c) takinekotfs 2023
+* Copyright (c) shiroinekotfs 2023
 *
 * You may not reproduce or distribute any code inside this file without the licenser's permission.
 * You may not copy, modify, steal, skid, or recreate any of the code inside this file.
 * You may not under any circumstance republish any code from this file as your own.
 * 
 * ALL TERMS STATED IN THE LINK BELOW APPLY ASWELL
-* https://github.com/takinekotfs/school-hacks/blob/main/LICENSE
+* https://github.com/shiroinekotfs/school-hacks/blob/main/LICENSE
 */
 
 /* THE UPDATE CHECKER IS ADDED DURING COMMIT PREP, THERE MAY BE REDUNDANT CODE, DO NOT TOUCH */
@@ -3570,7 +3570,7 @@
                 key: "x"
             })), React.createElement("div", {
                 id: styles.keys.credits
-            }, React.createElement(AntiBen, null, "GitHub - takinekotfs")), React.createElement("div", {
+            }, React.createElement(AntiBen, null, "GitHub - shiroinekotfs")), React.createElement("div", {
                 id: styles.keys.cheatList
             }, React.createElement(AntiBen, {
                 type: "span",
